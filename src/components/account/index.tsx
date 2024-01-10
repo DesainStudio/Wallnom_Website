e3877143691f475e26530ffcd15e6330010c0f0d
DesainStudio <105079980+DesainStudio@users.noreply.github.com>
2024-01-10 20:48:14 +0100

    test

--- src/components/account/index.tsx
+++ src/components/account/index.tsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 
 export function Account() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [typeUsername, checkusername] = useState('');
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+        console.log(typeUsername)
+    }, 1000);
+
+    return () => clearTimeout(delay)
+  }, [typeUsername])
 
   const handleFormSubmit = async (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     event.preventDefault();
   
-    const apiUrl = `http://37.221.93.114:25299/account?username=${encodeURIComponent(username)}&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+    const apiUrl = `http://37.221.93.114:25299/getaccount?username=${encodeURIComponent(username)}&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+    const apiUrlUsername = `http://37.221.93.114:25299/getaccount?username=${encodeURIComponent(username)}`; // &email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}
   
     try {
       const response = await fetch(apiUrl, {
@@ -40,15 +50,15 @@ export function Account() {
         <div id="main">
           <div className="input">
             <label>Username:</label>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input autoFocus autoComplete='off' type="text" value={username} onChange={(e) => setUsername(e.target.value) } onChangeCapture={(e) => checkusername(e.target.value) } />
           </div>
           <div className="input">
             <label>Email:</label>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input autoFocus autoComplete='off' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div className="input">
             <label>Password:</label>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input autoFocus autoComplete='off' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
           <a id='submit' href="#" onClick={handleFormSubmit}>
             Submit
